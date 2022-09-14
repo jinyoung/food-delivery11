e30f45ca171bc81f7f13ca32aa6014e2811401ca
Jinyoung Jang <jyjang@uengine.org>
2022-09-14 16:51:43 +0900

    commit

---
+++ front/src/main/java/food/delivery/domain/Order.java
@@ -0,0 +1,84 @@
+package food.delivery.domain;
+
+import food.delivery.domain.OrderPlaced;
+import food.delivery.domain.OrderCanceled;
+import food.delivery.FrontApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Order_table")
+@Data
+
+public class Order  {
+
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    
+    
+    
+    
+    
+    private Long id;
+    
+    
+    
+    
+    
+    private String foodId;
+    
+    
+    @ElementCollection
+    @ElementCollection
+    
+    private List<String> options;
+    
+    
+    
+    
+    
+    private String address;
+    
+    
+    
+    
+    
+    private String customerId;
+    
+    
+    
+    
+    
+    private String storeId;
+
+    @PostPersist
+    public void onPostPersist(){
+
+
+        OrderPlaced orderPlaced = new OrderPlaced(this);
+        orderPlaced.publishAfterCommit();
+
+    }
+    @PreRemove
+    public void onPreRemove(){
+
+
+        OrderCanceled orderCanceled = new OrderCanceled(this);
+        orderCanceled.publishAfterCommit();
+
+    }
+
+    public static OrderRepository repository(){
+        OrderRepository orderRepository = FrontApplication.applicationContext.getBean(OrderRepository.class);
+        return orderRepository;
+    }
+
+
+
+
+
+
+}
